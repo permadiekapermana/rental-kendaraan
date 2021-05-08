ba50bc100596280019e9086699a5f02c4cd99547
permadiekapermana <permadiekapermana@gmail.com>
2021-05-09 00:02:03 +0700

    dashboard pengelola

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -7,7 +7,7 @@ include('layout/sidebar.php');
 <!-- Main Content -->
 <div class="page-title">
     <div class="title_left">
-        <h3>Form Validation</h3>
+        <h3>Dashboard</h3>
     </div>
 
     <!-- <div class="title_right">
@@ -27,7 +27,7 @@ include('layout/sidebar.php');
     <div class="col-md-12 col-sm-12">
         <div class="x_panel">
             <div class="x_title">
-                <h2>Form validation <small>sub title</small></h2>
+                <h2>Dashboard</h2>
                 <!-- <ul class="nav navbar-right panel_toolbox">
                     <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                     </li>
@@ -44,7 +44,84 @@ include('layout/sidebar.php');
                 <div class="clearfix"></div>
             </div>
             <div class="x_content">
-                
+
+            <div class="row text-center mb-1">
+            <div class="col-sm-4">
+                <div class="card">
+                <div class="card-body">
+                    <?php 
+                        $sqlbayar = "SELECT kode_booking FROM booking WHERE status='Menunggu Pembayaran'";
+                        $querybayar = mysqli_query($koneksidb,$sqlbayar);
+                        $bayar=mysqli_num_rows($querybayar);
+                    ?>
+                    <h1 class="card-title"><?php echo htmlentities($bayar);?></h1>
+                    <p class="card-text">MENUNGU PEMBAYARAN</p>
+                    <a href="sewa_bayar.php" class="btn btn-sm btn-primary">RINCIAN <span class="fa fa-arrow-circle-right"></span></a>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card">
+                <div class="card-body">
+                    <?php 
+                        $sqlkonfir = "SELECT kode_booking FROM booking WHERE status='Menunggu Konfirmasi'";
+                        $querykonfir = mysqli_query($koneksidb,$sqlkonfir);
+                        $konfir=mysqli_num_rows($querykonfir);
+                    ?>
+                    <h1 class="card-title"><?php echo htmlentities($konfir);?></h1>
+                    <p class="card-text">MENUNGGU KONFIRMASI</p>
+                    <a href="sewa_konfirmasi.php" class="btn btn-sm btn-warning">RINCIAN <span class="fa fa-arrow-circle-right"></span></a>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card">
+                <div class="card-body">
+                    <?php 
+                        $sqlbelum = "SELECT kode_booking FROM booking WHERE status='Sudah Dibayar'";
+                        $querybelum = mysqli_query($koneksidb,$sqlbelum);
+                        $belum=mysqli_num_rows($querybelum);
+                    ?>
+                    <h1 class="card-title"><?php echo htmlentities($belum);?></h1>
+                    <p class="card-text">BELUM DIKEMBALIKAN</p>
+                    <a href="sewa_kembali.php" class="btn btn-sm btn-danger">RINCIAN <span class="fa fa-arrow-circle-right"></span></a>
+                </div>
+                </div>
+            </div>
+            </div>
+
+            <div class="row text-center mb-1">
+            <div class="col-sm-4">
+                <div class="card">
+                <div class="card-body">
+                    <?php 
+                        $sql1 = "SELECT id_mobil FROM mobil";
+                        $query1 = mysqli_query($koneksidb,$sql1);
+                        $totalvehicle=mysqli_num_rows($query1);
+                    ?>
+                    <h1 class="card-title"><?php echo htmlentities($totalvehicle);?></h1>
+                    <p class="card-text">JUMLAH MOBIL</p>
+                    <a href="pengelola_view_mobil.php" class="btn btn-sm btn-primary">RINCIAN <span class="fa fa-arrow-circle-right"></span></a>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card">
+                <div class="card-body">
+                    <?php 
+                        $sql2 = "SELECT kode_booking FROM booking";
+                        $query2 = mysqli_query($koneksidb,$sql2);
+                        $bookings=mysqli_num_rows($query2);
+                    ?>
+                    <h1 class="card-title"><?php echo htmlentities($bookings);?></h1>
+                    <p class="card-text">TOTAL SEWA</p>
+                    <a href="sewa.php" class="btn btn-sm btn-warning">RINCIAN <span class="fa fa-arrow-circle-right"></span></a>
+                </div>
+                </div>
+            </div>
+
+            </div>
+
             </div>
         </div>
     </div>
